85d645f1aecf8df8f9dce269728dfccf1f1d1b21
BitterMug <77463856+BitterMug@users.noreply.github.com>
2024-01-09 15:13:35 +0100

    icon, splashscreen

--- app/src/main/java/com/example/ak5pm/MainActivity.kt
+++ app/src/main/java/com/example/ak5pm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ak5pm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.example.ak5pm.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
